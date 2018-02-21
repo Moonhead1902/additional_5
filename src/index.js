85902f956a05abba0ce4d857b32a3c26af25bb99
Moonhead1902 <36646724+Moonhead1902@users.noreply.github.com>
2018-02-21 16:55:00 +0300

    Update index.js
    Taken from my lost github account: https://github.com/ArtsiomDeribo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var steck = [];
+
+
+    for (var i = 0; i < str.length; i++){
+        for (var j = 0; j < bracketsConfig.length; j++){
+            if (bracketsConfig[j][0] == bracketsConfig[j][1] && str[i] == bracketsConfig[j][0]){
+                if (steck[steck.length-1] == 0 || steck[steck.length-1] != str[i]){
+                    steck.push(str[i]);
+                    break;
+                } else {
+                    steck.pop();
+                    break;
+                }
+            }
+
+            if (str[i] == bracketsConfig[j][0]){
+                steck.push(str[i]);
+                break;
+            } else if (str[i] == bracketsConfig[j][1]){
+                if(steck.length != 0){
+                    if(steck[steck.length-1] == bracketsConfig[j][0]){
+                        steck.pop();
+                        break;
+                    } else return false;
+                } else return false;
+            }
+        }
+    }
+        return (steck.length == 0);
 }
